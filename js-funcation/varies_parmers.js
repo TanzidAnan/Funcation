3c40c3f7e127bb5c8541f7b4d8b6b127a241f5a9
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-06 18:11:23 +0600

    return of number funcation

--- js-funcation/varies_parmers.js
+++ js-funcation/varies_parmers.js
@@ -3,11 +3,28 @@ function eventSizedString(str){
     // console.log(size)
     console.log(str,size)
     if(size % 2 ===0){
-        console.log('event Number:',str,size)
+        console.log('event Number:',str,size);
+        return true
     }
     else{
-        console.log('odd Number:',str,size)
+        console.log('odd Number:',str,size);
+        return false
     }
 }
-eventSizedString('Dhaka')
-eventSizedString('faka')
+// eventSizedString('Dhaka')
+// eventSizedString('faka')
+
+
+function doubleOrTriple(number,doDouble){
+    if(doDouble){
+        const result =number * 2;
+        return result;
+    }
+    else{
+        const result =number* 3;
+        return result
+    }
+}
+
+console.log(doubleOrTriple(5,true));
+console.log(doubleOrTriple(5,false));

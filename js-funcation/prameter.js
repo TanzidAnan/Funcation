e23e1e1b5ef8e278a933acb7fa626da4281515f3
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-06 10:45:36 +0600

    sqare number

--- js-funcation/prameter.js
+++ js-funcation/prameter.js
@@ -4,6 +4,16 @@ function vaatKhao(){
     console.log('plate e khabar dhabar nao');
     console.log('gopth gopth kore khao')
 }
-vaatKhao();
-vaatKhao();
-vaatKhao();
+// vaatKhao();
+
+
+function sqare(number){
+    console.log('value of the number of prameter', number);
+    const borgo =number * number;
+    console.log(borgo)
+}
+sqare(4)
+console.log('------------------');
+sqare(15);
+console.log('====================');
+sqare(45)

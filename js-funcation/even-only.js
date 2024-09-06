18463a9e927489a0df724fdde32e140e1205da08
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-07 00:32:24 +0600

    function even numbers sum

--- js-funcation/even-only.js
+++ js-funcation/even-only.js
@@ -12,5 +12,23 @@ function evenNumberOnly(numbers){
 }
 
 const numbers =[20, 31, 42, 51, 62, 71, 82, 90];
-const events = evenNumberOnly(numbers);
-console.log(events)
+// const events = evenNumberOnly(numbers);
+// console.log(events)
+
+
+function sumOfNumbers(numbers){
+    let sum =0
+    for(const number of numbers){
+        
+        if(number %2 === 0){
+            // console.log(number);
+            sum =sum +number;
+        }
+        
+    }
+    return sum;
+    
+}
+
+const sum = sumOfNumbers(numbers);
+console.log("sum of the numbers is: ",sum);

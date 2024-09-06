f9534257a178ad449fba90d67855468a00807de4
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-06 10:35:16 +0600

    funcatin coll

---
+++ js-funcation/prameter.js
@@ -0,0 +1,9 @@
+function vaatKhao(){
+    console.log('hand wash kore aso');
+    console.log('boso');
+    console.log('plate e khabar dhabar nao');
+    console.log('gopth gopth kore khao')
+}
+vaatKhao();
+vaatKhao();
+vaatKhao();

c1e239fb4e74ef0a5f8e9653b43c1da72d3a76e8
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-06 10:51:13 +0600

    funcation sum

--- js-funcation/prameter.js
+++ js-funcation/prameter.js
@@ -12,8 +12,18 @@ function sqare(number){
     const borgo =number * number;
     console.log(borgo)
 }
-sqare(4)
-console.log('------------------');
-sqare(15);
-console.log('====================');
-sqare(45)
+// sqare(4)
+// console.log('------------------');
+// sqare(15);
+// console.log('====================');
+// sqare(45)
+
+
+
+function add(num1,num2){
+    const sum =num1 + num2;
+    console.log(sum)
+}
+
+add(20,60);
+add(20,45);

3acc0f5ae0d8f33382fde169f0422e7ca7b0eeb9
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-06 12:26:10 +0600

    function return

---
+++ js-funcation/retunt.js
@@ -0,0 +1,14 @@
+function tenTime(number){
+    const result =number *10;
+    return result;
+}
+
+
+function cutHaft(number){
+    const haft= number / 2;
+    return haft;
+}
+
+tenTime(5);
+const output = tenTime(5);
+console.log('Output:',output)

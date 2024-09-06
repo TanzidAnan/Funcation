968930bbce7f6e378e10ea382b97535542fe5417
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-06 17:29:38 +0600

    return price

---
+++ js-funcation/reacp.js
@@ -0,0 +1,14 @@
+function add(price1,price2){
+    const total =price1 + price2;
+    return total;
+}
+const baild =add(5, 10);
+// console.log(baild);
+
+
+function add1(price1, price2){
+    return price1 + price2;
+}
+
+const bail1 =add1(25, 36);
+console.log(bail1)

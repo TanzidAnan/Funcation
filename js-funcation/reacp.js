6a7e5cd27f592c85c02d32aa5dbb0adbf2f74f04
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-06 17:36:32 +0600

    doMath function

--- js-funcation/reacp.js
+++ js-funcation/reacp.js
@@ -11,4 +11,16 @@ function add1(price1, price2){
 }
 
 const bail1 =add1(25, 36);
-console.log(bail1)
+// console.log(bail1)
+
+
+function doMath(num1, num2){
+    const sum =num1 + num2;
+    const diff =num1 -num2;
+    const mult = sum * diff;
+    const result =mult / 2;
+    return result;
+}
+
+const total =doMath(10,5);
+console.log(total);

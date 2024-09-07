1d6de08579672225fca675b8a16d829ccf3992ba
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-07 13:01:32 +0600

    math funcation

--- js-problem-1/math.js
+++ js-problem-1/math.js
@@ -2,6 +2,15 @@
 
 
 const min = Math.min(25, 85, 78, 54, 65, 87, 14, 98, 78, 99, 45, 14,);
-console.log(min);
+// console.log(min);
 const max = Math.max(25, 85, 78, 54, 65, 87, 14, 98, 78, 99, 45, 14,);
-console.log(max);
+// console.log(max);
+
+console.log(Math.PI);
+console.log(Math.abs(-54-50));
+console.log(Math.round(2.9))
+console.log(Math.round(2.2))
+
+console.log(Math.random(10)*10)
+const ran = Math.round(Math.random(10)*10);
+console.log(ran)

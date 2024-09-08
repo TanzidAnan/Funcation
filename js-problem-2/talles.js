80141a87fa03e490e94ea15ebc9b796e43e8868e
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-08 09:49:47 +0600

    funcation max array

---
+++ js-problem-2/talles.js
@@ -0,0 +1,14 @@
+function getMax(numbers) {
+    // console.log(numbers)
+    let max =numbers[0];
+    for (const number of numbers) {
+        if(number >max){
+            max =number
+        }
+    }
+    return max
+}
+
+const hight = [54, 32, 85, 45, 65, 75, 36, 55];
+const max = getMax(hight);
+console.log('max number', max)

f4ba8cacfdf3c294f7f138aedf3b91ae82c40cae
TanzidAnan <tanzid.anan125@gmail.com>
2024-09-08 09:07:23 +0600

    funcation max numbere

---
+++ js-problem-2/max.js
@@ -0,0 +1,22 @@
+const tanzid = 80;
+const marzan =70;
+
+if(tanzid> marzan){
+    // console.log('win')
+}
+else{
+    console.log('secdeon win')
+}
+
+
+function getMex(num1, num2){
+    if(num1> num2){
+        return num1
+    }
+    else{
+        return num2;
+    }
+}
+
+const max = getMex(45, 69);
+console.log("get naumber of :", max)

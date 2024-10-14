513285fdd8f9b38c18ea9adfdf5374a43048a6c7
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-14 12:34:12 +0600

    object

--- arrow-funcation/index.js
+++ arrow-funcation/index.js
@@ -1,4 +1,12 @@
 const addFast =(x) =>{
     console.log(x)
 }
-addFast(30)
+// addFast(30);
+
+const person ={
+    name:'tanzid',
+    age:20,
+    isStudent:true
+}
+
+console.log(person.isStudent && 'Not a Student')

0d205c439701405630b33f34b853ec74b9781dab
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-14 15:31:33 +0600

    arrow

--- arrow-funcation/index.js
+++ arrow-funcation/index.js
@@ -1,15 +1,21 @@
-const addFast =(x) =>{
+const addFast = (x) => {
     console.log(x)
 }
 // addFast(30);
 
-const person ={
-    name:'tanzid',
-    age:20,
-    isStudent:true,
-    address:'Dhaka'
+const person = {
+    name: 'tanzid',
+    age: 20,
+    isStudent: true,
+    address: 'Dhaka'
 
 }
-console.log(person?.address ||"N/A")
+console.log(person?.address || "N/A")
 
-console.log(person.isStudent && 'Not a Student')
+console.log(person.isStudent && 'Not a Student');
+
+const num = [12, 65, 32, 46, 97, 46, 36, 45, 65, 44, 36, 47, 36, 74, 96, 41, 85, 11];
+
+const [x,y,...z] =num;
+console.log(x,y);
+console.log(z)

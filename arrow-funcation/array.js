5ebcb8b1e51cdbd5bfcda702df7f9e96f21b8718
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-14 19:10:46 +0600

    reduce

--- arrow-funcation/array.js
+++ arrow-funcation/array.js
@@ -1,3 +1,7 @@
 const number = [12, 42, 36, 36, 41, 35,];
-const arr =number.reduce((previous,current) =>previous+ current,0);
+const arr = number.reduce((previous, current) => {
+   const totle =(previous + current);
+   console.log(totle)
+   return totle
+}, 0);
 console.log(arr)

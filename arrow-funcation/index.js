fbfc2aec0812e0abd8d15bd9ae99c6200702289e
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-14 15:48:53 +0600

    object

--- arrow-funcation/index.js
+++ arrow-funcation/index.js
@@ -24,3 +24,4 @@ const num2 =[...num];
 num2.push(56,41,65);
 console.log(num2);
 console.log(num)
+
---
+++ arrow-funcation/object.js
@@ -0,0 +1,7 @@
+const obj ={
+    name:'Hello',
+    age:30
+}
+const obj2 =obj;
+obj2.address ='Dhaka';
+console.log(obj2)

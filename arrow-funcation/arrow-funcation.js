d2577174daff7f7a2877c193d03c61fd19556d7b
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-15 12:26:15 +0600

    for

--- arrow-funcation/arrow-funcation.js
+++ arrow-funcation/arrow-funcation.js
@@ -1,10 +1,16 @@
-const adds =(x,y,colback) =>{
+const adds = (x, y, colback) => {
     console.log('Hello');
     console.log(typeof colback);
-    colback(x,y)
+    colback(x, y)
 };
-const time =(x,y) =>{
-    console.log(x+y)
-    return x=y;
+const time = (x, y) => {
+    console.log(x + y)
+    return x = y;
 }
-adds(5,6,time)
+// adds(5,6,time)
+
+const numbers = [20, 30, 65, 96, 45, 85, 69];
+for(let i=0 ; i<numbers.length; i++){
+    const number =numbers[i]*2
+    console.log(number)
+}

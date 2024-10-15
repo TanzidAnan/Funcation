ade984dda7dfbe6ab507a5c7b43847805805ea89
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-15 22:10:44 +0600

    commit

--- arrow-funcation/arrow-funcation.js
+++ arrow-funcation/arrow-funcation.js
@@ -13,8 +13,8 @@ const numbers = [20, 30, 65, 96, 45, 85, 69];
 
 const number1 =[...numbers];
 number1.push(21,52,85);
-console.log(number1);
-console.log(numbers)
+// console.log(number1);
+// console.log(numbers)
 
 for(let i=0 ; i<numbers.length; i++){
     const number =numbers[i]*2
@@ -27,6 +27,6 @@ const person ={
     address:'Barisal',
     isTime:true
 }
-console.log(person['age']);
+// console.log(person['age']);
 
-console.log(`My name ${person.name},My age ${person.age} my address ${person.address}`);
+console.log(`My name ${person.name}, My age ${person.age} my address ${person.address}`);

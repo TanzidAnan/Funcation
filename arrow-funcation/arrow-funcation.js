725b2e52da74b0c9f97f9fb31a1a39983f1158e3
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-15 22:01:26 +0600

    arrow

--- arrow-funcation/arrow-funcation.js
+++ arrow-funcation/arrow-funcation.js
@@ -10,7 +10,12 @@ const time = (x, y) => {
 // adds(5,6,time)
 
 const numbers = [20, 30, 65, 96, 45, 85, 69];
+
+const number1 =[...numbers];
+number1.push(21,52,85);
+console.log(number1)
+
 for(let i=0 ; i<numbers.length; i++){
     const number =numbers[i]*2
-    console.log(number)
+    // console.log(number)
 }

fdf967d6d36db6465345808ea5002640e255b1f7
TanzidAnan <tanzid.anan125@gmail.com>
2024-10-16 20:35:01 +0600

    fecth

---
+++ arrow-funcation/fetch.js
@@ -0,0 +1,10 @@
+const person ={
+    name:'Tanzid',
+    age:20,
+    address:'Barisal',
+    isTime:true
+}
+
+const personJSON =JSON.stringify(person);
+console.log(person);
+console.log(personJSON)
